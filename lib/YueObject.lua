4318d4d3cd0dde8a8cce324c738897c983fc6b04
Raymond Cheung <rcheung@gavekal.com>
2017-05-09 17:07:36 +0800

    Add YueObject, which is extended from deepmind/classic

---
+++ lib/YueObject.lua
@@ -0,0 +1,115 @@
+--[[
+ * @author Raymond Cheung <korekontakt@gmail.com>
+ * 
+ * @copyright Copyright (c) 2016, Raymond Cheung.
+--]]
+
+-- https://github.com/deepmind/classic
+-- https://github.com/deepmind/classic/blob/master/classic/Class.lua
+-- https://www.lua.org/pil/16.1.html
+-- https://docs.oracle.com/javase/tutorial/java/javaOO/variables.html
+
+local classic = require("classic");
+local YueObject = classic.class("YueObject");
+
+--[[
+  Access Modifiers
+    private = true: Methods are not extendable.
+    private = false: Methods are extendable.
+--]]
+YueObject._modifier = {private = true};
+
+function YueObject:_init(opt)
+  self = opt or {};   -- create object if user does not provide one
+end
+
+function YueObject:new(opt)
+  if (type(opt) == "table") then
+    for k, v in pairs(opt) do
+      self[k] = v;
+    end
+  end
+  return self;
+end
+
+function YueObject:clone()
+  local new_obj = {};
+  if (type(self) ~= "table") then
+    new_obj = self;
+    return new_obj;
+  end
+  for k, v in pairs(self) do
+    new_obj[k] = v;
+  end
+  local self_meta = getmetatable(self);
+  setmetatable(new_obj, self_meta);
+  return new_obj;
+end
+
+-- Extend another class methods
+function YueObject:extend(klass)
+  if (type(klass) == "string") then
+    klass = classic.getClass(klass);
+  end
+  assert(klass ~= nil, "invalid class include");
+  local klassAttributes = rawget(klass, "_classAttributes") or {};
+  local klassModifiers = klassAttributes._modifier or {};
+  if (klassModifiers.private) then
+    return false;
+  end
+
+  -- Methods provided by the mixin are copied to the including class.
+  local methods = rawget(self:class(), "_methods");
+  local otherMethods = rawget(klass, "_methods");
+  for name, func in pairs(otherMethods) do
+    if (methods[name] == nil) then
+      methods[name] = func;
+    end
+  end
+  
+  return true;
+end
+
+-- Get a class Attribute
+function YueObject:getter(attr)
+  if (type(attr) == "string") then    
+    return self[attr];
+  else
+    return nil;
+  end
+end
+
+-- Set a class Attribute
+function YueObject:setter(attr, value)
+  if (type(attr) == "string" and type(self[attr]) == type(value)) then    
+    self[attr] = value;
+  end
+end
+
+function YueObject:trycatch(funcLambda, err_msg)
+  local psts, perr;
+  if (type(err_msg) ~= "string") then
+    err_msg = "";
+  end
+  if (type(funcLambda) == "function") then
+    psts, perr = pcall(funcLambda);
+    if (not psts) then
+      print("*****");
+      print("ERROR: " .. err_msg, perr);
+      print("*****");
+    end
+  end
+  return psts, perr;
+end
+
+function YueObject:test()
+  print("Loading " .. self:class():name() .. ":test()...");
+  print("Methods:", self:class():methods());
+  
+  local parent = self:class():parent();
+  if (parent ~= nil) then
+    print("Parent:", parent);
+  end
+end
+
+return YueObject;
