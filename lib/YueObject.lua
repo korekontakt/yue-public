4f728c5766bc8c774779c4efbd669b63285b3278
Raymond Cheung <rcheung@gavekal.com>
2017-05-11 10:51:41 +0800

    Remove comma and add Test Case for the private modifier

--- lib/YueObject.lua
+++ lib/YueObject.lua
@@ -9,110 +9,110 @@
 -- [Programming in Lua - Classes](https://www.lua.org/pil/16.1.html)
 -- [Oracle - The Java Tutorials - Declaring Member Variables](https://docs.oracle.com/javase/tutorial/java/javaOO/variables.html)
 
-local classic = require("classic");
-local YueObject = classic.class("YueObject");
+local classic = require("classic")
+local YueObject = classic.class("YueObject")
 
 --[[
   Access Modifiers
     private = true: Methods are not extendable.
     private = false: Methods are extendable.
 --]]
-YueObject._modifier = {private = true};
+YueObject._modifier = {private = true}
 
 function YueObject:_init(opt)
-  self = opt or {};   -- create object if user does not provide one
+  self = opt or {}   -- create object if user does not provide one
 end
 
 function YueObject:new(opt)
   if (type(opt) == "table") then
     for k, v in pairs(opt) do
-      self[k] = v;
+      self[k] = v
     end
   end
-  return self;
+  return self
 end
 
 function YueObject:clone()
-  local new_obj = {};
+  local new_obj = {}
   if (type(self) ~= "table") then
-    new_obj = self;
-    return new_obj;
+    new_obj = self
+    return new_obj
   end
   for k, v in pairs(self) do
-    new_obj[k] = v;
+    new_obj[k] = v
   end
-  local self_meta = getmetatable(self);
-  setmetatable(new_obj, self_meta);
-  return new_obj;
+  local self_meta = getmetatable(self)
+  setmetatable(new_obj, self_meta)
+  return new_obj
 end
 
 -- Extend another class methods
 function YueObject:extend(klass)
   if (type(klass) == "string") then
-    klass = classic.getClass(klass);
+    klass = classic.getClass(klass)
   end
-  assert(klass ~= nil, "invalid class include");
-  local klassAttributes = rawget(klass, "_classAttributes") or {};
-  local klassModifiers = klassAttributes._modifier or {};
-  if (klassModifiers.private) then
-    return false;
+  assert(klass ~= nil, "invalid class include")
+  local klassAttributes = rawget(klass, "_classAttributes") or {}
+  local klassModifiers = klassAttributes._modifier or {}
+  if (type(klassModifiers) == "table" and klassModifiers.private) then
+    return false
   end
 
   -- Methods provided by the mixin are copied to the including class.
-  local methods = rawget(self:class(), "_methods");
-  local otherMethods = rawget(klass, "_methods");
+  local methods = rawget(self:class(), "_methods")
+  local otherMethods = rawget(klass, "_methods")
   for name, func in pairs(otherMethods) do
     if (methods[name] == nil) then
-      methods[name] = func;
+      methods[name] = func
     end
   end
   
-  return true;
+  return true
 end
 
 -- Get a class Attribute
 function YueObject:getter(attr)
   if (type(attr) == "string") then    
-    return self[attr];
+    return self[attr]
   else
-    return nil;
+    return nil
   end
 end
 
 -- Set a class Attribute
 function YueObject:setter(attr, value)
   if (type(attr) == "string" and type(self[attr]) == type(value)) then    
-    self[attr] = value;
+    self[attr] = value
   end
 end
 
 function YueObject:trycatch(funcLambda, err_msg)
-  local psts, perr;
+  local psts, perr
   if (type(err_msg) ~= "string") then
-    err_msg = "";
+    err_msg = ""
   end
   if (type(funcLambda) == "function") then
-    psts, perr = pcall(funcLambda);
+    psts, perr = pcall(funcLambda)
     if (not psts) then
-      print("*****");
-      print("ERROR: " .. err_msg, perr);
-      print("*****");
+      print("*****")
+      print("ERROR: " .. err_msg, perr)
+      print("*****")
     end
   end
-  return psts, perr;
+  return psts, perr
 end
 
 function YueObject:test()
-  local base = self:class();
-  print("Loading " .. base:name() .. ":test()...");
-  print("Methods:", base:methods());
+  local base = self:class()
+  print("Loading " .. base:name() .. ":test()...")
+  print("Methods:", base:methods())
 
   if (self._parent ~= nil) then
-    local parent = base:parent();
+    local parent = base:parent()
     if (parent ~= nil) then
-      print("Parent:", parent);
+      print("Parent:", parent)
     end
   end
 end
 
-return YueObject;
+return YueObject
